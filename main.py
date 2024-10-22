2153ddef4c464265e725b493bc9bf5358ceef93b
Muhammad Naveed Arshad <55968054+naveedarshad01@users.noreply.github.com>
2024-10-23 00:37:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Vercel!"})
+    return jsonify({"message": "Hello, ML Experts!"})
 
 # Make sure the app runs in production
 if __name__ == '__main__':
